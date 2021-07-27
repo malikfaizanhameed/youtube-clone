13eff72cb6a96236d83d508a01595ebe81788845
Malik Faizan Hameed <faizan@Maliks-MacBook-Air.local>
2021-07-27 17:24:42 +0500

    header added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Header from "./Header";
 function App() {
   return (
     <div className="App">
-      <h1>Hello, let's make a YouTube clone....!</h1>
-
       <Header />
       {/* Header */}
       {/* Sidebar */}
