41b06e6e0f7f90e0b7d162534606a7b34366ce35
Malik Faizan Hameed <faizan@Maliks-MacBook-Air.local>
2021-07-27 15:09:30 +0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBXT9qr6t0ePxy4Ht6LjGzmucsk9zmHFM0",
+  authDomain: "clone-210c5.firebaseapp.com",
+  projectId: "clone-210c5",
+  storageBucket: "clone-210c5.appspot.com",
+  messagingSenderId: "912889049406",
+  appId: "1:912889049406:web:619c41a18bfd26f755bc6f",
+  measurementId: "G-213HVJ0Z9S",
+};
