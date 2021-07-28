43ccc992f2c5031937be247916108bbe888afb77
Malik Faizan Hameed <faizan@Maliks-MacBook-Air.local>
2021-07-28 17:45:44 +0500

    sidebar added

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./RecommendedVideos.css";
+
+const RecommendedVideos = (props) => {
+  return <div className="recommendedVideos">I am the Videos section</div>;
+};
+
+export default RecommendedVideos;
---
+++ src/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarRow from "./SidebarRow";
+import HomeIcon from "@material-ui/icons/Home";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+
+const Sidebar = (props) => {
+  return (
+    <div className="sidebar">
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={WhatshotIcon} title="Trending" />
+      <SidebarRow Icon={SubscriptionsIcon} title="Subscriptions" />
+    </div>
+  );
+};
+
+export default Sidebar;
